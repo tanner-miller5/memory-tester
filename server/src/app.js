1ce176f1967339e1b4467825031264c3e33fb216
Tanner Miller <tanner_miller@myyahoo.com>
2025-08-18 16:33:44 -0700

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -10,9 +10,15 @@ const { sequelize, Test } = require('./models')
 
 const app = express();
 
+const corsOptions = {
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    credentials: false // Set to false when using origin: '*'
+};
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', apiRouter);
